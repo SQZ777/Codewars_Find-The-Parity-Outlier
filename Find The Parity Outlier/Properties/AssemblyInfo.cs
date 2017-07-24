d3995bfbd6ee2157cb2a8dd1fe311febddc5e162
TitanDarren <sqz777isme@gmail.com>
2017-07-24 15:50:59 +0800

    Initial project

---
+++ Find The Parity Outlier/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Find The Parity Outlier")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Titansoft")]
+[assembly: AssemblyProduct("Find The Parity Outlier")]
+[assembly: AssemblyCopyright("Copyright © Titansoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("da964e77-f6e5-49a8-bb68-e212e1964a71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Find The Parity Outlier/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Find_The_Parity_Outlier
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+
+        }
+    }
+}
