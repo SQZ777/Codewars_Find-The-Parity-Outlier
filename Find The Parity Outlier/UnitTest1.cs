2b4ee564f51e0515b1dbfa31360b2525b86e151d
TitanDarren <sqz777isme@gmail.com>
2017-07-24 15:59:55 +0800

    add test case: Input_0_1_Maxint_Should_Be_0 and failed.

--- Find The Parity Outlier/UnitTest1.cs
+++ Find The Parity Outlier/UnitTest1.cs
@@ -7,11 +7,21 @@ namespace Find_The_Parity_Outlier
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_0_1_Maxint_Should_Be_0()
         {
+            var kata = new Kata();
+            int[] input = { 0, 1, int.MaxValue };
+            int actual = kata.Find(input);
+            int expected = 0;
+            Assert.AreEqual(expected, actual);
+        }
+    }
 
-
-
+    public class Kata
+    {
+        public int Find(int[] input)
+        {
+            throw new NotImplementedException();
         }
     }
 }
